44b15100474316b694b7f2a9acd62644f4511261
pdvonzoo <a01088570194@gmail.com>
2019-07-19 04:24:23 +0900

    Feat: add Shop component

--- src/core/Shop.js
+++ src/core/Shop.js
@@ -2,3 +2,20 @@ import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
 import { getProducts } from "./apiCore";
 import Card from "./Card";
+
+const Shop = () => {
+  return (
+    <Layout
+      title="Home Page"
+      description="Node React E-commnerce App"
+      className="container-fluid"
+    >
+      <div className="row">
+        <div className="col-4">left sidbar</div>
+        <div className="col-8">right</div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Shop;
