d87ad75c5d001a8a76a93b225de10b0d8275495c
pdvonzoo <a01088570194@gmail.com>
2019-07-18 22:22:13 +0900

    Feat: update login component

--- src/auth/index.js
+++ src/auth/index.js
@@ -12,3 +12,16 @@ export const signup = user => {
     .then(response => response.json())
     .catch(err => console.log(err));
 };
+
+export const signin = user => {
+  return fetch(`${API}/signin`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(user)
+  })
+    .then(response => response.json())
+    .catch(err => console.log(err));
+};
