aa2656e98916b3cc7c646f925b622c9eb7acf124
pdvonzoo <a01088570194@gmail.com>
2019-07-19 01:50:58 +0900

    Feat: add fetching create category route

--- src/admin/AddCategory.js
+++ src/admin/AddCategory.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Layout from "../core/Layout";
 import { isAuthenticated } from "../auth";
 import { Link } from "react-router-dom";
+import { createCategory } from "./apiAdmin";
 
 const AddCategory = () => {
   const [name, setName] = useState("");
@@ -19,6 +20,14 @@ const AddCategory = () => {
     e.preventDefault();
     setError("");
     setSuccess(false);
+    createCategory(user._id, token, { name }).then(data => {
+      if (data.error) {
+        setError(true);
+      } else {
+        setError("");
+        setSuccess(true);
+      }
+    });
   };
   const newCategoryForm = () => (
     <form onSubmit={clickSubmit}>
@@ -30,19 +39,45 @@ const AddCategory = () => {
           onChange={handleChange}
           value={name}
           autoFocus
+          required
         />
         <button className="btn btn-outline-primary">Create Category</button>
       </div>
     </form>
   );
 
+  const showSuccess = () => {
+    if (success) {
+      return <h3 className="text-success">{name} is created</h3>;
+    }
+  };
+
+  const showError = () => {
+    if (error) {
+      return <h3 className="text-danger">Category is should be unique</h3>;
+    }
+  };
+
+  const goBack = () => (
+    <div className="mt-5">
+      <Link to="/admin/dashboard" className="text-warning">
+        Back to Dashboard
+      </Link>
+    </div>
+  );
+
   return (
     <Layout
       title="Add a new category"
-      description={`G'dat ${name}, ready to add a new category?`}
+      description={`G'dat ${user.name}, ready to add a new category?`}
     >
       <div className="row">
-        <div className="col-md-8 offset-md-2">{newCategoryForm()}</div>
+        <div className="col-md-8 offset-md-2">
+          {showSuccess()}
+          {showError()}
+          {newCategoryForm()}
+          {goBack()}
+        </div>
       </div>
     </Layout>
   );
