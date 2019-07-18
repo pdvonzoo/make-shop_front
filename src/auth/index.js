75effffbdfacfbdb96f15a95e399f692c590cd10
pdvonzoo <a01088570194@gmail.com>
2019-07-18 22:27:10 +0900

    Feat: 토큰 localstorage에 저장

--- src/auth/index.js
+++ src/auth/index.js
@@ -25,3 +25,10 @@ export const signin = user => {
     .then(response => response.json())
     .catch(err => console.log(err));
 };
+
+export const authenticate = (data, next) => {
+  if (typeof window !== undefined) {
+    localStorage.setItem("jwt", JSON.stringify(data));
+    next();
+  }
+};
--- src/user/Signin.js
+++ src/user/Signin.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link, Redirect } from "react-router-dom";
 import Layout from "../core/Layout";
-import { signin } from "../auth";
+import { signin, authenticate } from "../auth";
 
 const Signin = () => {
   const [values, setValues] = useState({
@@ -25,9 +25,11 @@ const Signin = () => {
       if (data.error) {
         setValues({ ...values, error: data.error, loading: false });
       } else {
-        setValues({
-          ...values,
-          redirectToRefferrer: true
+        authenticate(data, () => {
+          setValues({
+            ...values,
+            redirectToRefferrer: true
+          });
         });
       }
     });
