190ea14badccbaa5ead07841cb725eb2550be20c
pdvonzoo <a01088570194@gmail.com>
2019-07-19 12:16:16 +0900

    Feat: Filter with price range

--- src/core/RadioBox.js
+++ src/core/RadioBox.js
@@ -1,15 +1,19 @@
 import React, { useState, useEffect, Fragment } from "react";
 
-const RadioBox = ({ prices }) => {
+const RadioBox = ({ prices, handleFilters }) => {
   const [value, setValue] = useState(0);
 
-  const handleChange = () => {};
+  const handleChange = event => {
+    handleFilters(event.target.value);
+    setValue(event.target.value);
+  };
 
   return prices.map((p, i) => (
     <div key={i} className="list-unstyled">
       <input
         onChange={handleChange}
         value={`${p._id}`}
+        name={p}
         type="radio"
         className="mr-2 ml-4"
       />
