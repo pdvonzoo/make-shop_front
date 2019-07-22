9668d55c467dca2f645969cf19472cd85f258d38
pdvonzoo <a01088570194@gmail.com>
2019-07-22 13:01:15 +0900

    Refactoring Checkout button

--- src/core/Checkout.js
+++ src/core/Checkout.js
@@ -4,6 +4,7 @@ import { getProducts } from "./apiCore";
 import Card from "./Card";
 import { isAuthenticated } from "../auth";
 import { Link } from "react-router-dom";
+
 const Checkout = ({ products }) => {
   const getTotal = () => {
     return products.reduce((currentValue, nextValue) => {
@@ -11,16 +12,20 @@ const Checkout = ({ products }) => {
     }, 0);
   };
 
+  const showCheckout = () => {
+    return isAuthenticated() ? (
+      <button className="btn btn-success">Checkout</button>
+    ) : (
+      <Link to="/signin">
+        <button className="btn btn-primary">Sign in to checkcout</button>
+      </Link>
+    );
+  };
+
   return (
     <div>
       <h2>Total: ${getTotal()}</h2>
-      {isAuthenticated() ? (
-        <button className="btn btn-success">Checkout</button>
-      ) : (
-        <Link to="/signin">
-          <button className="btn btn-primary">Sign in to checkcout</button>
-        </Link>
-      )}
+      {showCheckout()}
     </div>
   );
 };
