a34c81db2f7495b82afbf7b3f470e03dc2e07527
pdvonzoo <a01088570194@gmail.com>
2019-07-22 15:08:20 +0900

    Feat: add braintree api

--- src/core/apiCore.js
+++ src/core/apiCore.js
@@ -60,3 +60,16 @@ export const listRelated = productId => {
     .then(response => response.json())
     .catch(err => console.log(err));
 };
+
+export const getBraintreeClientToken = (userId, token) => {
+  return fetch(`${API}/braintree/getToken/${userId}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`
+    }
+  })
+    .then(response => response.json())
+    .catch(err => console.log(err));
+};
