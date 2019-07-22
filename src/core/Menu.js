4445990d8146f2ea96b6480eeabd5c34f6c8ab4f
pdvonzoo <a01088570194@gmail.com>
2019-07-22 12:11:33 +0900

    Feat: cart items total in menu

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
 import { signout, isAuthenticated } from "../auth";
+import { itemTotal } from "./cartHelpers";
 
 const isActive = (history, path) => {
   if (history.location.pathname === path) {
@@ -25,6 +26,18 @@ const Menu = ({ history }) => (
           to="/shop"
         >
           Shop
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          className="nav-link"
+          style={isActive(history, "/cart")}
+          to="/cart"
+        >
+          Cart{" "}
+          <sup>
+            <small className="cart-badge">{itemTotal()}</small>
+          </sup>
         </Link>
       </li>
       {isAuthenticated() && isAuthenticated().user.role === 0 && (
